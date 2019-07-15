b8fe22069c91eea5b219d434301f86d5b8946e9f
kaLi-lhj <hwejae94@likelion.org>
2019-07-15 21:26:43 +0900

    test

---
+++ daepa/urls.py
@@ -0,0 +1,26 @@
+"""daepa URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import login.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',login.views.home, name="home"),
+    path('login.html',login.views.login, name="login"),
+    path('register.html',login.views.register, name="register"),
+    path('signup.html', login.views.signup, name='join'),
+]
